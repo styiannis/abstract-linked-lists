bf391e48faf755a3eeb3f0ebc039b6710e1bd2d7
Yiannis <1515939+styiannis@users.noreply.github.com>
2025-01-29 20:51:04 +0200

    feat(core): doubly linked list

---
+++ src/core/doubly-linked-list/list.ts
@@ -0,0 +1,176 @@
+import { IDoublyLinkedList } from '../../types';
+
+/**
+ * Creates a new doubly linked list instance.
+ *
+ * @typeParam L - The type of the list.
+ */
+export function create<L extends IDoublyLinkedList>() {
+  return { size: 0, head: null, tail: null } as L;
+}
+
+/**
+ * Clears a doubly linked list by removing all nodes.
+ *
+ * @typeParam L - The type of the list.
+ * @param instance - The list instance.
+ */
+export function clear<L extends IDoublyLinkedList>(instance: L) {
+  instance.size = 0;
+  instance.head = null;
+  instance.tail = null;
+}
+
+/**
+ * Retrieves the node at the specified index in a doubly linked list.
+ *
+ * Time Complexity: `O(n)`, where `n` is the number of nodes in the list divided by 2.
+ * Space Complexity: `O(1)`
+ *
+ * @typeParam L - The type of the list.
+ * @param instance - The list instance.
+ * @param index - The zero-based `index` of the node to retrieve.
+ * @returns The node at the specified `index`, or `undefined` if the `index` is out of bounds.
+ */
+export function nodeAt<L extends IDoublyLinkedList>(
+  instance: L,
+  index: number
+) {
+  if (0 > index || instance.size - 1 < index) {
+    return undefined;
+  }
+
+  if (index < instance.size - 1 - index) {
+    let node = instance.head as NonNullable<L['head']>;
+
+    for (let i = 0; i < index && node.next; i++) {
+      node = node.next;
+    }
+
+    return node;
+  }
+
+  let node = instance.tail as NonNullable<L['head']>;
+
+  for (let i = instance.size - 1; node.previous && i > index; i--) {
+    node = node.previous;
+  }
+
+  return node;
+}
+
+/**
+ * Removes and returns the last node from a doubly linked list.
+ *
+ * Time Complexity: `O(1)`
+ * Space Complexity: `O(1)`
+ *
+ * @typeParam L - The type of the list.
+ * @param instance - The list instance.
+ * @returns The removed node, or `undefined` if the list is empty.
+ */
+export function popNode<L extends IDoublyLinkedList>(instance: L) {
+  if (!instance.tail) {
+    return;
+  }
+
+  const lastNode: NonNullable<L['head']> = instance.tail;
+  const beforeLast: L['head'] = lastNode.previous;
+
+  if (!beforeLast) {
+    instance.head = null;
+    instance.tail = null;
+  } else {
+    lastNode.previous = null;
+    beforeLast.next = null;
+    instance.tail = beforeLast;
+  }
+
+  instance.size--;
+
+  return lastNode;
+}
+
+/**
+ * Adds a node to the end of a doubly linked list.
+ *
+ * Time Complexity: `O(1)`
+ * Space Complexity: `O(1)`
+ *
+ * @typeParam L - The type of the list.
+ * @param instance - The list instance.
+ * @param node - The node to add.
+ */
+export function pushNode<L extends IDoublyLinkedList>(
+  instance: L,
+  node: NonNullable<L['head']>
+) {
+  if (!instance.tail) {
+    instance.head = node;
+    instance.tail = node;
+  } else {
+    node.previous = instance.tail;
+    instance.tail.next = node;
+    instance.tail = node;
+  }
+
+  instance.size++;
+}
+
+/**
+ * Removes and returns the first node from a doubly linked list.
+ *
+ * Time Complexity: `O(1)`
+ * Space Complexity: `O(1)`
+ *
+ * @typeParam L - The type of the list.
+ * @param instance - The list instance.
+ * @returns The removed node, or `undefined` if the list is empty.
+ */
+export function shiftNode<L extends IDoublyLinkedList>(instance: L) {
+  if (!instance.head) {
+    return;
+  }
+
+  const first: NonNullable<L['head']> = instance.head;
+  const second: L['head'] = first.next;
+
+  if (!second) {
+    instance.head = null;
+    instance.tail = null;
+  } else {
+    first.next = null;
+    second.previous = null;
+    instance.head = second;
+  }
+
+  instance.size--;
+
+  return first;
+}
+
+/**
+ * Adds a node to the beginning of a doubly linked list.
+ *
+ * Time Complexity: `O(1)`
+ * Space Complexity: `O(1)`
+ *
+ * @typeParam L - The type of the list.
+ * @param instance - The list instance.
+ * @param node - The node to add.
+ */
+export function unshiftNode<L extends IDoublyLinkedList>(
+  instance: L,
+  node: NonNullable<L['head']>
+) {
+  if (!instance.head) {
+    instance.head = node;
+    instance.tail = node;
+  } else {
+    node.next = instance.head;
+    instance.head.previous = node;
+    instance.head = node;
+  }
+
+  instance.size++;
+}
--- tests/core.test.ts
+++ tests/core.test.ts
@@ -1,4 +1,4 @@
-import { singlyLinkedList } from '../src';
+import { doublyLinkedList, singlyLinkedList } from '../src';
 import { isValidObjectInstance } from './test-utils';
 
 describe('Core', () => {
@@ -25,6 +25,38 @@ describe('Core', () => {
         expect(node.next).toBe(null);
         expect(next.next).toBe(null);
       });
+
+      it('Doubly linked list', () => {
+        const previous = doublyLinkedList.node.create();
+        const next = doublyLinkedList.node.create();
+        const node = doublyLinkedList.node.create(previous, next);
+
+        expect(
+          isValidObjectInstance(node, 'doubly-linked-list-node') &&
+            isValidObjectInstance(node.previous, 'doubly-linked-list-node') &&
+            isValidObjectInstance(node.next, 'doubly-linked-list-node')
+        ).toBe(true);
+
+        expect(previous.previous).toBe(null);
+        expect(previous.next).toBe(node);
+
+        expect(node.previous).toBe(previous);
+        expect(node.next).toBe(next);
+
+        expect(next.previous).toBe(node);
+        expect(next.next).toBe(null);
+
+        doublyLinkedList.node.detach(node);
+
+        expect(previous.previous).toBe(null);
+        expect(previous.next).toBe(next);
+
+        expect(node.previous).toBe(null);
+        expect(node.next).toBe(null);
+
+        expect(next.previous).toBe(previous);
+        expect(next.next).toBe(null);
+      });
     });
   });
 
@@ -34,6 +66,16 @@ describe('Core', () => {
         const list = singlyLinkedList.list.create();
 
         expect(isValidObjectInstance(list, 'singly-linked-list')).toBe(true);
+
+        expect(list.size).toBe(0);
+        expect(list.head).toBe(null);
+        expect(list.tail).toBe(null);
+      });
+
+      it('Doubly linked list', () => {
+        const list = doublyLinkedList.list.create();
+
+        expect(isValidObjectInstance(list, 'doubly-linked-list')).toBe(true);
 
         expect(list.size).toBe(0);
         expect(list.head).toBe(null);
